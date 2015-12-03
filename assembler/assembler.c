164e15a4b2dce6982e5818b0485951922791122c
Fabugo <fabugo@gmail.com>
2015-12-02 22:58:22 -0300

    oh meu deus, treinando manipulamento de arquivos

--- assembler/assembler.c
+++ assembler/assembler.c
@@ -2,26 +2,56 @@
 #include "string.h"
 
 int main(int argc, char const *argv[]) {
-    FILE *file;
-    file = fopen("program", "r");
-    char instruction[6],
-                par1[4],
-                par2[4],
-                par3[4];
+    FILE *file;             //ponteiro do arquivo a ser manipulado
+    char instruction[10],   //instrucao do programa
+                par1[10],   //1 argumento do programa
+                par2[10],   //2 argumento do programa
+                par3[10],   //3 argumento do programa
+                bin [32];   //saida do sistema
 
+    int args,               //numero total de argumentos que a instrucao tem
+        type;               //1 operacoes logicas
+                            //2 operacoes aritmeticas
+                            //3 operacoes com constantes
+                            //4 operacoes de memoria
+                            //5 operacoes de desvios
+    char * pchr;            //ponteiro que auxilia contagem de argumentos
+    char zero = '0';
+    int i = 0;
+    for(i;i<=31;i++){
+        printf("%s\n",bin);
+        fflush(stdin);
+        bin[i] = NULL;
+        bin[i] = zero;
+    }
+
+    //iniciando leitura
+    file = fopen("assembler\\program", "r");
+
+    //identificando o tipo de operacao
     fscanf(file,"%s",instruction);
     if(!strcmp(instruction,"add")){
+        type = 2;
         fscanf(file,"%s",par1);
-        fscanf(file,"%s",par2);
-        fscanf(file,"%s",par3);
+        pchr = strchr(par1,',');
+        if(pchr!=NULL){
+            fscanf(file,"%s",par2);
+            args = 2;
+            pchr = strchr(par2,',');
+            if(pchr!=NULL){
+                fscanf(file,"%s",par3);
+                args = 3;
+            }
+        }else{
+            args = 1;
+        }
     }
     fclose(file);
+    //leitura completa
 
-    file = fopen("bin", "w");
-    fprintf(file, "%s %s %s %s","0000 0000 0000 0000 0001 0000 0010 0011",par1,par2,par3);
-
-
-
-    system("pause");
+    //inicio de escrita
+    file = fopen("simulation\\bin", "w");
+    fprintf(file, "%s\n", bin);
+    //system("pause");
     return fclose(file);
 }
