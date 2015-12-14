302f472b8c7a623f3ad918f421432ac6e95128d6
Mathob <mathob.ce@outlook.com>
2015-12-14 09:23:07 -0300

    Correcao no busca binaria

--- assembler/input/binary.asm
+++ assembler/input/binary.asm
@@ -8,44 +8,44 @@
 	loadlit r1,0
 	loadlit r2,10
 	loadlit r3,5
-	;r4 registrador temporario
-	;r5 registrador da posicao corrente
-	;r6 registrador para a saida da execucao
+	loadlit r4, 1023			;Ponteiro para o endereço de memória para a saida
+	;r5 registrador temporario
+	;r6 registrador da posicao corrente
 
-	MAIN:	sub r4,r2,r1
+	MAIN:	sub r5,r2,r1
 			jf.zero SPLIT		;Se 'end' nao for igual a 'start'
 			;nop
 
-			load r4,r0			;Neste ponto, o vetor possui tamanho 1
-			sub r4,r4,r3
+			load r5,r0			;Neste ponto, o vetor possui tamanho 1
+			sub r5,r5,r3
 			jt.zero ENDS		;Se 'vetor[start]' for igual ao valor buscado
 			;nop
-			loadlit r5,-1		;Se não for, o elemento não existe no vetor
+			loadlit r6,-1		;Se não for, o elemento não existe no vetor
 			j ENDC
 
-	SPLIT:	asr r4,1			;Divide ('end'-'start') por 2
-			add r5,r4,r1 		;Soma com o offset do start
-			add r0,r0,r5		;Move o ponteiro do vetor para a possicao corrente
-			load r4,r0			;Carrega o valor da posicao atual do vetor
-			sub r4,r3,r4
+	SPLIT:	asr r5,1			;Divide ('end'-'start') por 2
+			add r6,r5,r1 		;Soma com o offset do start
+			add r0,r0,r6		;Move o ponteiro do vetor para a possicao corrente
+			load r5,r0			;Carrega o valor da posicao atual do vetor
+			sub r5,r3,r5
 			jt.zero ENDC		;Se o valor corrente é igual ao buscado
 			;nop
 			jt.neg LEFT			;Se o valor corrente for maior que o buscado
 			;nop
-			passa r1,r5			;O inicio passa a ser a posicao corrente
+			passa r1,r6			;O inicio passa a ser a posicao corrente
 			j MAIN
 
-	LEFT:	passa r2,r5			;O fim passa a ser a posicao corrente
-			sub r0,r0,r5		;Move o ponteiro do vetor para o inicio dele
+	LEFT:	passa r2,r6			;O fim passa a ser a posicao corrente
+			sub r0,r0,r6		;Move o ponteiro do vetor para o inicio dele
 			j MAIN
 
-	ENDS:	passa r6,r1
+	ENDS:	passa r5,r1
 			j END
 
-	ENDC:	passa r6,r5
+	ENDC:	passa r5,r6
 			j END
 
-	END:	;Como finaliza??
+	END:	store r4,r5
 
 .dseg
 	VET:	.word 1
