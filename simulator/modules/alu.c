fc0bde3a02a69f0bd160f7e583e3494e41d7edc8
Fabugo <fabugo@gmail.com>
2016-01-17 17:54:25 -0300

    iniciando ula

--- simulator/modules/alu.c
+++ simulator/modules/alu.c
@@ -8,7 +8,59 @@ void ALU_start(){
 }
 
 void OP_ALU(char operation)){
-    switch (strEquals(operation, "10011") {
-        case /* value */:
+    if(strEquals(operation,"00000")){
+        
+    }else if(strEquals(operation,"00001")){
+
+    }else if(strEquals(operation,"00011")){
+
+    }else if(strEquals(operation,"00100")){
+
+    }else if(strEquals(operation,"00101")){
+
+    }else if(strEquals(operation,"00110")){
+
+    }else if(strEquals(operation,"01000")){
+
+    }else if(strEquals(operation,"01001")){
+
+    }else if(strEquals(operation,"10000")){
+
+    }else if(strEquals(operation,"10001")){
+
+    }else if(strEquals(operation,"10010")){
+
+    }else if(strEquals(operation,"10011")){
+
+    }else if(strEquals(operation,"10100")){
+
+    }else if(strEquals(operation,"10101")){
+
+    }else if(strEquals(operation,"10110")){
+
+    }else if(strEquals(operation,"10111")){
+
+    }else if(strEquals(operation,"11000")){
+
+    }else if(strEquals(operation,"11001")){
+
+    }else if(strEquals(operation,"11010")){
+
+    }else if(strEquals(operation,"11011")){
+
+    }else if(strEquals(operation,"11100")){
+
+    }else if(strEquals(operation,"11101")){
+
+    }else if(strEquals(operation,"11110")){
+
+    }else if(strEquals(operation,"11111")){
+
+    }else if(strEquals(operation,"01100")){
+
+    }else if(strEquals(operation,"01101")){
+
+    }else if(strEquals(operation,"01110")){
+
     }
 }
